191a392da6b2703c5ae27f584fbcf221e1d3db05
sayeramesh <sayerameshbabu@gmail.com>
2020-11-05 18:17:22 +0530

    added div function to s1 branch

--- src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
+++ src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
@@ -45,7 +45,7 @@ public class Calculator {
 		
 		Calculator cal = new Calculator(first, second);
 		
-		String output = String.format("\n*** Your Results ***\n\nFirst: %d\nSecond: %d\n\nSum : %d\nDifference : %d\nProduct : %d\n\n", cal.first, cal.second, cal.addFucn(first, second), cal.subFucn(first, second), cal.mulFucn(first, second), cal.divFucn(first, second));
+		String output = String.format("\n*** Your Results ***\n\nFirst: %d\nSecond: %d\n\nSum : %d\nDifference : %d\nProduct :  %d\nDivision: %d\n\n", cal.first, cal.second, cal.addFucn(first, second), cal.subFucn(first, second), cal.mulFucn(first, second), cal.divFucn(first, second));
 		System.out.println(output);
 	}
 	
